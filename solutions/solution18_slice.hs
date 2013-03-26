79deb3d5868e7ff1e4dbbcdcf546cdbe9ae85005
Hiroshi Kori <kori39@gmail.com>
2013-03-26 00:26:58 -0700

    Improved match express.

--- solutions/solution18_slice.hs
+++ solutions/solution18_slice.hs
@@ -1,6 +1,6 @@
 slice :: [a] -> Int -> Int -> [a]
 slice _ _ 0 = []
-slice (x:xs) n m | n>m = []
-                 | n<=1 && m>0 = x : rest
-                 | otherwise = rest
+slice (x:xs) n m | n>m  = []
+                 | n>1 = rest
+                 | otherwise = x:rest
                  where rest = slice xs (n-1) (m-1)
