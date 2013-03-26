d8d9fffd488708d46128632445964bb7ba28e984
Hiroshi Kori <kori39@gmail.com>
2013-03-26 01:23:27 -0700

    Refactored utilizing mod behavior.

--- solutions/solution19_rotate.hs
+++ solutions/solution19_rotate.hs
@@ -1,4 +1,3 @@
 rotate :: [a] -> Int -> [a]
-rotate xs n = drop border xs ++ take border xs 
-              where len = length xs
-                    border = (len+n) `mod` len
+rotate xs n = drop m xs ++ take m xs 
+              where m = n `mod` length xs
