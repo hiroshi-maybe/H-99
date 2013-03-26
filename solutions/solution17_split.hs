f90fae78335f22741782bac1d0ebf0cfeedb50f9
Hiroshi Kori <kori39@gmail.com>
2013-03-25 23:44:09 -0700

    Corrected result type.

--- solutions/solution17_split.hs
+++ solutions/solution17_split.hs
@@ -1,13 +1,4 @@
-split :: [a] -> Int -> [[a]]
-split xs 0 = [[], xs]
-split (x:xs) n = (x: head' rest) : tail' rest
-               where rest = split xs $ n-1
-
--- Do not use any predefined predicates
-head' :: [a] -> a
-head' [] = error "empty list"
-head' (x:xs) = x
-
-tail' :: [a] -> [a]
-tail' [] = error "empty list"
-tail' (x:xs) = xs
+split :: [a] -> Int -> ([a],[a])
+split xs 0 = ([], xs)
+split (x:xs) n = (x:f, s)
+                 where (f,s) = split xs $ n-1
