e7249c64fe16d6b9cc309dbe98a4537cdaad64c0
Hiroshi Kori <kori39@gmail.com>
2013-03-28 01:22:59 -0700

    Used splitAt instead of take and drop.

--- solutions/solution25_rnd-permu.hs
+++ solutions/solution25_rnd-permu.hs
@@ -3,6 +3,6 @@ import System.Random
 rnd_permu :: [a] -> IO [a]
 rnd_permu [] = return []
 rnd_permu xs = do rnd <- randomRIO (0, length xs -1)
-                  let combed = take rnd xs ++ drop (rnd+1) xs
+                  let combed = let (ys,z:zs) = splitAt rnd xs in ys++zs
                   rest <- rnd_permu combed
                   return ((xs!!rnd):rest)
