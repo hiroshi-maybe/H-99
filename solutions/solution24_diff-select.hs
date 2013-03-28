6bf97f9b581ed7d7cca48b0226c2e55c3d669dda
Hiroshi Kori <kori39@gmail.com>
2013-03-28 00:56:01 -0700

    Removed redundant 'do'.

--- solutions/solution24_diff-select.hs
+++ solutions/solution24_diff-select.hs
@@ -3,7 +3,7 @@ import System.Random
 diff_select :: Int -> Int -> IO [Int]
 diff_select c m = diff_select' c [1..m]
                   where diff_select' :: Int -> [Int] -> IO [Int]
-                        diff_select' 0 _ = do return []
+                        diff_select' 0 _ = return []
                         diff_select' c xs = do rand <- randomRIO (0,length xs -1)
                                                let combed = take rand xs ++ drop (rand+1) xs
                                                rest <- diff_select' (c-1) combed
