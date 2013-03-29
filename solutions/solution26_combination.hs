b73eb07443a3c77a58c928e5b24ef53a0b65e241
Hiroshi Kori <kori39@gmail.com>
2013-03-28 23:52:20 -0700

    Removed filter and added if.

--- solutions/solution26_combination.hs
+++ solutions/solution26_combination.hs
@@ -1,6 +1,7 @@
 combinations :: (Eq a) =>Int -> [a] -> [[a]]
 combinations _ [] = [[]]
 combinations 0 _ = [[]]
-combinations n (x:xs) = filter ((==n).length) (inc ++ combinations n xs)
-                          where inc = do cand <- combinations (n-1) xs
+combinations n (x:xs) = x_inc ++ x_exc
+                        where x_inc = do cand <- combinations (n-1) xs
                                          return (x:cand)
+                              x_exc = if n>length xs then [] else combinations n xs
