51c9e83491cde7b4b0f69eacb75d330942ab6456
Hiroshi Kori <kori39@gmail.com>
2013-03-30 15:11:53 -0700

    Integrated square root functin.

--- solutions/solution31_is-prime.hs
+++ solutions/solution31_is-prime.hs
@@ -1,5 +1,3 @@
 isPrime :: Int -> Bool
-isPrime n = null $ filter (\x -> n `mod` x==0) [2..squareRoot n]
-
-squareRoot :: Int -> Int
-squareRoot = floor . sqrt . fromIntegral
+isPrime n = null $ filter (\x -> n `mod` x==0) [2..squareRoot]
+            where squareRoot = floor.sqrt.fromIntegral $ n
