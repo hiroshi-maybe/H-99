5e320914002f59283022a521f7872f9a7157481c
Hiroshi Kori <kori39@gmail.com>
2013-03-30 03:52:05 -0700

    Removed unnecessary tuple.

--- solutions/solution28_length-freq-sort.hs
+++ solutions/solution28_length-freq-sort.hs
@@ -4,6 +4,6 @@ lsort :: [[a]] -> [[a]]
 lsort = sortBy (\a b -> (length a) `compare` (length b))
 
 lfsort :: [[a]] -> [[a]]
-lfsort = map snd.(sortBy (\(n,_) (m,_) -> n `compare` m)).freq.lsort
-            where freq :: [[a]] -> [(Int,[[a]])]
-                  freq = map (\x -> (length x,x)).(groupBy (\a b -> (length a) == (length b)))
+lfsort = concat.lsort.freq.lsort
+         where freq :: [[a]] -> [[[a]]]
+               freq = groupBy (\a b -> (length a) == (length b))
