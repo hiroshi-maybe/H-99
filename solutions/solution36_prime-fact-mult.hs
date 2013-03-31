5d3b4fc167cfeac5cae31604c4efa830dadac1a1
Hiroshi Kori <kori39@gmail.com>
2013-03-30 18:09:42 -0700

    Solved problem 36.

---
+++ solutions/solution36_prime-fact-mult.hs
@@ -0,0 +1,11 @@
+import Data.List (groupBy)
+
+-- Copy of solution 35.
+primeFactors :: Int -> [Int]
+primeFactors 1 = []
+primeFactors n = (factor:) $ primeFactors $ n `div` factor
+                 where isFactor = (==0).(mod n)
+                       factor = head $ filter isFactor [2..n]
+
+prime_factors_mult :: Int -> [(Int,Int)]
+prime_factors_mult = map (\xs -> (head xs,length xs)) . groupBy (==) . primeFactors
