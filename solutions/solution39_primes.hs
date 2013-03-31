0ed1bddcb625873fef0378e829f565bbf02daf28
Hiroshi Kori <kori39@gmail.com>
2013-03-31 01:39:39 -0700

    Fixed a bug that small primes are not found.

--- solutions/solution39_primes.hs
+++ solutions/solution39_primes.hs
@@ -1,3 +1,3 @@
 primesR :: Int -> Int -> [Int]
-primesR l u = filter (\x -> not $ any (\y -> x `mod` y == 0) [2..sqrtNum]) [l..u]
-              where sqrtNum = truncate $ sqrt $ fromIntegral u
+primesR l u = filter (\x -> not $ any (\y -> x `mod` y == 0) [2..sqrtNum x]) [l..u]
+              where sqrtNum = truncate.sqrt.fromIntegral
