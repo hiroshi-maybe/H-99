da1b8ad54c97605bf40dd0038a38e248c7804fd1
Hiroshi Kori <kori39@gmail.com>
2013-04-04 02:07:15 -0700

    Used

--- solutions/solution_54-60.hs
+++ solutions/solution_54-60.hs
@@ -5,6 +5,5 @@ data Tree a = Empty | Branch a (Tree a) (Tree a) deriving (Show, Eq)
 cbalTree :: Int -> [Tree Char]
 cbalTree 0 = [Empty]
 cbalTree n = [ Branch 'x' left right | lNodes <- bNum n, left <- cbalTree lNodes, right <- cbalTree (n-1-lNodes)]
-             where bNum m | odd m = [base]
-                          | otherwise = [base, base+1]
-                          where base = (m-1) `div` 2
+             where bNum m = [base .. base+ (m-1) `mod` 2]
+                            where base = (m-1) `div` 2
